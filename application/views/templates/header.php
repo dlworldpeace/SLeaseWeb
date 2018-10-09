d4f673bca01c58c431432071099af4c0382337ef
dlworldpeace <xdl_line@hotmail.com>
2018-10-09 22:39:07 +0800

    add style.css to our project folder.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,6 +2,7 @@
     <head>
         <title>Slease</title>
         <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
