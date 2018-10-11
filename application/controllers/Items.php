e7c9aa60d716d7cc23ef95a6a6417a37ee3a4e8e
dlworldpeace <xdl_line@hotmail.com>
2018-10-11 11:05:52 +0800

    fix wrong deletion from earlier. get category id in items/create function.

--- application/controllers/Items.php
+++ application/controllers/Items.php
@@ -2,7 +2,6 @@
     class Items extends CI_Controller {
         public function index() {
             $data['title'] = 'Items on lease: ';
-            
 
             $data['items'] = $this->item_model->get_items();
             
@@ -13,25 +12,29 @@
 
         public function detail($item_id) {
             $data['items'] = $this->item_model->get_items($item_id);
-            $data['category'] = $this->category_model ->get_category($Cat_id);
 
             if(empty($data['items'])) {
                 show_404();
             }
             $data['item'] = reset($data['items']);
+            $data['category'] = $this->category_model ->get_category($data['item']['categories']);
 
             $this->load->view('templates/header');
             $this->load->view('items/detail', $data);
             $this->load->view('templates/footer');
         }
 
-        public function create() {
+        public function create($msg = FALSE) {
             $data['title'] = 'Lease a new item';
+            if($msg !== FALSE) {	
+                $data['error'] = $msg['error'];	
+            }
 
             $this->form_validation->set_rules('owner', 'Owner', 'required');
             $this->form_validation->set_rules('item_name', 'Item_name', 'required');
             $this->form_validation->set_rules('description', 'Description', 'required');
             $this->form_validation->set_rules('minbid', 'Minbid', 'required');
+            //$this->form_validation->set_rules('user_file', 'Image', 'required');
             $this->form_validation->set_rules('pickup_location', 'Pickup_location', 'required');
             $this->form_validation->set_rules('return_location', 'Return_location', 'required');
             $this->form_validation->set_rules('fromdate', 'Fromdate', 'required');
@@ -44,8 +47,36 @@
                 $this->load->view('items/create', $data);
                 $this->load->view('templates/footer');
             } else {
-                print_r($this->input);
-                $this->item_model->create_item();
+                // $config['upload_path']          = './uploads/';
+                // $config['allowed_types']        = 'gif|jpg|png';
+                // $config['max_size']             = 100;	
+                // $config['max_width']            = 150;	
+                // $config['max_height']           = 150;	
+                 // $this->load->library('upload', $config);	
+                 // if($this->upload->do_upload() && $this->item_model->create_item()) {	
+                //     redirect('items');	
+                // } else {	
+                //     $msg = array('error' => $this->upload->display_errors());	
+                //     print_r($msg);	
+                //     //redirect('items/create', $msg);	
+                // }	
+                if($this->item_model->create_item()) {	
+                    redirect('items');	
+                }	
+            }	
+        }	
+
+        public function search() {	
+            $keyword = $this->input->post('searchBy');	
+            $data['items'] = $this->item_model->search_items($keyword);	
+            $data['title'] = 'Items with the keyword: '.$keyword;	
+             $this->load->view('templates/header');	
+            $this->load->view('items/index', $data);	
+            $this->load->view('templates/footer');	
+        }	
+
+        public function delete($item_id){	
+            if($this->item_model->delete_item($item_id)) {
                 redirect('items');
             }
         }
